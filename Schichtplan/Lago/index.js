01c1227e3551d5a5ecbffd383cf23da158176fb2
Unknown <dean.klopsch@students.unibe.ch>
2019-07-20 12:04:11 +0200

    logger and some comments

--- Schichtplan/Lago/index.js
+++ Schichtplan/Lago/index.js
@@ -1,28 +1,43 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var plan = require('./scripts/generatePlan.js');
+
+//imports
+const express = require('express');
+const path = require('path');
+const app = express();
+const plan = require('./scripts/generatePlan');
 var month="";
+const logger = require('./scripts/logger');
 
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
+
+//a happy little logger
+//uncomment to enable
+//app.use(logger);
+
+//simple index direction
 app.get('/index.html', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+//form to generate new worker
 app.get('/newWorker.html', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public','newWorker.html'));
-})
+});
 
+//download the plan
 app.get('/download', (req,res)=>{
+    //send the plan with the right month as name
     res.download('plan.xlsx', month+'.xlsx');
-})
-//get the file
+});
+
+//generate plan and make it ready for download
 app.get('/plan/:date', function (req, res) {
 
     plan.saveDate(req.params.date);
-    // res.sendFile(path.join(__dirname, './try.xlsx'));
+    //redirect to download page to give file time to generate
     res.sendFile(path.join(__dirname, 'public','download.html'));
+    //set the variable month to ensure the downloaded file has the right name
     month= plan.month(req.params.date);
 });
 var PORT = process.env.PORT || 5000;
